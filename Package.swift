38702f60cb752cddd5c8b355e335685e941477c5
nerzh <emptystamp@gmail.com>
2025-10-16 23:40:15 +0200

    up dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,10 +8,10 @@ let name: String = "TonSdkSwift"
 var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/nerzh/swift-regular-expression", .upToNextMajor(from: "0.2.3")),
     .package(url: "https://github.com/bytehubio/BigInt", exact: "5.3.0"),
-    .package(url: "https://github.com/nerzh/swift-extensions-pack", .upToNextMajor(from: "1.19.1")),
-    .package(url: "https://github.com/apple/swift-crypto", "1.0.0" ..< "3.0.0"),
+    .package(url: "https://github.com/nerzh/swift-extensions-pack", .upToNextMajor(from: "2.0.3")),
+    .package(url: "https://github.com/apple/swift-crypto", .upToNextMajor(from: "3.0.0")),
     .package(url: "https://github.com/bytehubio/CryptoSwift", .upToNextMajor(from: "1.8.1")),
-    .package(url: "https://github.com/nerzh/swift-net-layer", .upToNextMajor(from: "1.5.0")),
+    .package(url: "https://github.com/nerzh/swift-net-layer", .upToNextMajor(from: "1.6.2")),
 ]
 
 var mainTarget: [Target.Dependency] = [
